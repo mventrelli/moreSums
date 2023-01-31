9446768dcc6e373183aa4ce83265331984b9effd
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-01-31 11:50:31 -0600

    Initial Commit

---
+++ moreSums/moreSumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  moreSumsApp.swift
+//  moreSums
+//
+//  Created by IIT PHYS 440 on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct moreSumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
